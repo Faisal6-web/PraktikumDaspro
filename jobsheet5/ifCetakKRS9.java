8c13b7083db8057a06c94ec4d0162bce4f378e5e
Faisal6-web <rizkyfaisal886@gmail.com>
2025-10-08 16:59:20 +0700

    Percobaan1

--- jobsheet5/ifCetakKRS9.java
+++ jobsheet5/ifCetakKRS9.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class ifCetakKRS9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    System.out.println("---CETAK KRS SIAKAD---");
-    System.out.print("Apakah ukt sudah lunas? (true/false): ");
-    boolean uktLunas = sc.nextBoolean();
-if (uktLunas) {
-    System.out.println("Pembayaran UKT Terverifikasi");
-    System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-}
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? ( true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+    if (uktLunas); {
+        System.out.println("Pembayaran UKT terverifikasi");
+        System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
     }
+    }
+    
 }
