1bf3c23de1fa1023b8d3e8af057ae76bdcd975c9
Faisal6-web <rizkyfaisal886@gmail.com>
2025-10-09 10:53:42 +0700

    percobaan 1

--- jobsheet5/ifCetakKRS9.java
+++ jobsheet5/ifCetakKRS9.java
@@ -7,10 +7,11 @@ public class ifCetakKRS9 {
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? ( true/false): ");
         boolean uktLunas = sc.nextBoolean();
-    if (uktLunas); {
+    if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silakan cetak KRS dan minta tanda tangan DPA:");
+    } else {
+       System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
     }
     }
-    
 }
