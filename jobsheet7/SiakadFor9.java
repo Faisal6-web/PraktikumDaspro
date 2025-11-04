33f7d18815e9606ec2a8ad650db25d05209d708a
Faisal6-web <rizkyfaisal886@gmail.com>
2025-11-04 23:50:51 +0700

    ModifikasiPercobaan 1

--- jobsheet7/SiakadFor9.java
+++ jobsheet7/SiakadFor9.java
@@ -5,6 +5,7 @@ public class SiakadFor9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
     double nilai, tertinggi = 0, terendah=100;
+    int JmlLulus = 0, JmlTidakLulus = 0;
     for (int i=1; i <= 10; i++){
         System.out.print("Masukkan nilai Mahasiswa ke-" + i +": ");
         nilai = sc.nextDouble();
@@ -13,9 +14,17 @@ public class SiakadFor9 {
         }
         if ( nilai < terendah){
             terendah = nilai;
-        }    
+        }
+        
+        if ( nilai >=60){
+            JmlLulus++;
+        }else{
+            JmlTidakLulus++;
+        }
     }
     System.out.println("Nilai tertinggi" + tertinggi);
     System.out.println("Nilai terendah" + terendah);
+    System.out.println("Jumlah Mahasiswa Lulus"  + JmlLulus);
+    System.out.println("Jumlah Mahasiswa Tidak Lulus" + JmlTidakLulus);
     }
 }
