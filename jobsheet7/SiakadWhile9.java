b4015769b8c2b63f8ea76f0df1fed2e3ec0e7681
Faisal6-web <rizkyfaisal886@gmail.com>
2025-11-05 00:49:17 +0700

    ModifikasiPercobaan 2

--- jobsheet7/SiakadWhile9.java
+++ jobsheet7/SiakadWhile9.java
@@ -16,6 +16,7 @@ while (i < jml){
 
     if(nilai > 80 && nilai <=100){
         System.out.println("Nilai mahasiswa ke-" + (i+1)+ " adalah A");
+        System.out.println("Bagus, Pertahankan Nilainya!");
     }else if ( nilai >73 && nilai <=80) {
         System.out.println("Nilai mahasiswa ke-" + (i+1) + " adlaah B+");
     }else if (nilai >65 && nilai <=73) {
