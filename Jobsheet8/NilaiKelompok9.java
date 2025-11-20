404b1f5b8d7feb295cf4ef77b4e66baf96b880e5
Faisal6-web <rizkyfaisal886@gmail.com>
2025-11-20 09:06:30 +0700

    Percobaan 4

---
+++ Jobsheet8/NilaiKelompok9.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class NilaiKelompok9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=1, nilai;
+        float totalNilai, rataNilai;
+    while (i <=6) {
+        System.out.println("Kelompok " + i);
+
+        totalNilai = 0;
+        for (int j =1; j<=5; j++) {
+            System.out.print("Nilai dari kelompok Penilai " + j + ":");
+            nilai = sc.nextInt();
+            totalNilai += nilai;
+        }
+
+        rataNilai = totalNilai / 5;
+        System.out.println("Nilai Rata-Rata" + rataNilai);
+        i++;
+
+    }
+    }
+}
