7c4cfafa145293852eb32c8689ba11600b215647
Faisal6-web <rizkyfaisal886@gmail.com>
2025-12-04 06:15:27 +0700

    Tugas 1

--- jobsheet7/Tugas1Jobsheet7.java
+++ jobsheet7/Tugas1Jobsheet7.java
@@ -4,27 +4,47 @@ public class Tugas1Jobsheet7 {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-
-    double Tiket=50.000, totalTiket, totalPenjualan, i;
+    int hargaTiket = 50000, totalTiketTerjual = 0;
+    double totalUangMasuk = 0;
     String namaPenonton ;
-    i=1;
-    if (Tiket > 10 ) {
-        System.out.println("Penonton Mendapat Diskon 15%" + Tiket*15/100);
-    }else if (Tiket > 4) {
-        System.out.println("Penonton Mendapat Diskon 10%" + Tiket*10/100);
-    }else 
-     System.out.println("Penonton Tidak Mendapat Diskon");
-
-    do {
-        System.out.println("Masukkan Nama Penonton Ke-" + i + ":");
-        namaPenonton = sc.nextLine();
-        System.out.println("Masukkan jumlah Tiket yang dibeli" + Tiket);
-        Tiket = sc.nextDouble();
-        totalTiket = Tiket ;
-        i++;
-    }while (true) ;
+    System.out.println("=== Program Bioskop ===");
         
-    }
+    while (true) {
+        System.out.print("Masukkan jumlah tiket yang dibeli:");
+        int jumlahTiket = sc.nextInt();
+        
+        if(jumlahTiket == 0) {
+            System.out.println("Transaksi Selesai. Hitung total tiket terjual");
+            break;
+        }
+        
+         if(jumlahTiket < 0) {
+            System.out.println("Nilai Tidak valid, masukkan ulang");
+            continue;
+         }
 
+         double totalHarga = jumlahTiket*hargaTiket;
+         double diskon = 0;
+
+         if(jumlahTiket >10 ) {
+            diskon = totalHarga *15/100;
+            System.out.println("Selamat anda mendapatkan diskon 15%");
+         }else if (jumlahTiket >4 ) {
+            diskon = totalHarga * 10/100;
+            System.out.println("Selamat anda mendapatkan diskon 10%");
+         }
+
+         double bayar = totalHarga - diskon;
+         System.out.println("Total bayar pelanggan ini: Rp" + bayar);
+         System.out.println("---------------------------");
+
+         totalTiketTerjual += jumlahTiket;
+         totalUangMasuk += bayar;
+
+         System.out.println(" \n=== Laporan Penjualan Harian ===");
+         System.out.println(" Total Tiket Terjual :" + totalTiketTerjual);
+         System.out.println(" Total Pendapat Sehari : Rp " + totalUangMasuk);
     }
+    }
+}
 
