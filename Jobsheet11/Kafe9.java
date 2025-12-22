f19ae3b6bebca959d458eba6cae4e6a17e397606
Faisal6-web <rizkyfaisal886@gmail.com>
2025-12-22 19:02:39 +0700

    Percobaan 3

--- Jobsheet11/Kafe9.java
+++ Jobsheet11/Kafe9.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kafe9 {
     public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, "+ namaPelanggan+ "!");
@@ -24,9 +26,28 @@ public class Kafe9 {
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===============================");
-        System.out.println("Silahkan pilih menu yang Anda Inginkan");   
+        System.out.println("Silahkan pilih menu yang Anda Inginkan");  
     }
-   public static void main(String[] args) {
-    menu("Budi", true, "DISKON30");
-   }
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int hargaTotal = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp"+hargaTotal );
+    }
+    
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int [] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    
+    }
+  
+   
 }
